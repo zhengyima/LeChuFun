b13073823b567d497766aa9b1eadb5fd18aa2b13
lmerengues <317958662@qq.com>
2017-12-11 19:33:12 +0800

    Initial Commit

---
+++ pages/detail/detail.js
@@ -0,0 +1,64 @@
+
+//logs.js
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      '../../images/girl2.jpeg',
+      '../../images/girl.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=99be91f1067ce820af8235607706813a&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091538.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1493698928333&di=ae56672831512cc7d4cd1e26d31269aa&imgtype=0&src=http%3A%2F%2Fimg.tupianzj.com%2Fuploads%2Fallimg%2F160412%2F9-160412091540.jpg'
+    ],
+    indicatorDots: true,
+
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    markers: [{
+      iconPath: "../../images/logo.jpg",
+      id: 0,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      width: 50,
+      height: 50
+    }],
+    polyline: [{
+      points: [{
+        longitude: 113.3245211,
+        latitude: 23.10229
+      }, {
+        longitude: 113.324520,
+        latitude: 23.21229
+      }],
+      color: "#FF0000DD",
+      width: 2,
+      dottedLine: true
+    }],
+    controls: [{
+      id: 1,
+      iconPath: '../../images/logo.jpg',
+      position: {
+        left: 0,
+        top: 300 - 50,
+        width: 50,
+        height: 50
+      },
+      clickable: true
+    }]
+  },
+  to_detail:function(){
+    wx.navigateTo({
+      url: "../detail/detail"
+    })
+  },
+
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  }
+})
